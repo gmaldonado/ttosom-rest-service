46096d86c6faa36ca39b14d6c3379126de589e87
Gonzalo Maldonado <gmaldonadol@outlook.com>
2016-03-06 22:48:10 -0300

    classification added

--- src/main/java/com/ttosom/rest/RestController.java
+++ src/main/java/com/ttosom/rest/RestController.java
@@ -5,7 +5,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
-
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -19,6 +20,7 @@ import com.ttosom.neuron.Neuron;
 import com.ttosom.neuron.NodeValue;
 
 import weka.classifiers.Evaluation;
+import weka.core.Instance;
 import weka.core.Instances;
 import static com.ttosom.TTOSOMUtils.*;
 
@@ -29,13 +31,14 @@ public class RestController{
 	
 	private TTOSOM ttosom;
 	private Instances dataSet;
+	private Distance distanceFunction;
  
 	private  void initializeTTOSOM(BasicRequest jsonRequest) throws Exception{
 		Map<String, Distance> distances = initializeDistancesMap();
 		
 		dataSet = readArffFromURL(jsonRequest.getDataSetUrl()); 
 		
-		Distance distanceFunction = distances.get(jsonRequest.getDistanceFunction());
+		distanceFunction = distances.get(jsonRequest.getDistanceFunction());
 		int initialRadius = jsonRequest.getInitialRadius();
 		int finalRadius = jsonRequest.getFinalRadius();
 		double initialLearningRate = jsonRequest.getInitialLearningRate();
@@ -49,13 +52,70 @@ public class RestController{
 		ttosom.buildClassifier(dataSet);
 	}
 	
-	@RequestMapping(value = "/", method=RequestMethod.POST)
-	public ResponseEntity<List<Neuron>> callRegularFlow(@RequestBody BasicRequest jsonRequest) throws Exception {
+	@RequestMapping(value = "/classification", method=RequestMethod.POST)
+	public ResponseEntity<JSONObject> classification(@RequestBody BasicRequest jsonRequest) throws Exception {
+		
 		initializeTTOSOM(jsonRequest);
-		List<Neuron> neurons = ttosom.getNeuronList();
-		return new ResponseEntity<>(neurons,HttpStatus.OK); 
+        Instances classifiedData = null;
+        Instances unlabeledData = new Instances(dataSet,0);
+        
+        extractUnlabeledData(unlabeledData);
+        
+        classifiedData = classifyUnlabeledData(unlabeledData,classifiedData);
+		
+        JSONObject jsonObject = generateInstancesAsJson(classifiedData);
+		return new ResponseEntity<JSONObject>(jsonObject,HttpStatus.OK); 
+	}
+
+	private void extractUnlabeledData(Instances unlabeledData) {
+		for(int i=0;i<dataSet.numInstances();i++){
+            if(dataSet.instance(i).classIsMissing()){
+            	unlabeledData.add(dataSet.instance(i));
+            }
+        }
 	}
 	
+	private Instances classifyUnlabeledData( Instances unlabeledData, Instances classifiedData) {
+		
+		if(unlabeledData.numInstances()>0 && unlabeledData.numInstances()!=dataSet.numInstances()){
+				//the data to be classified is the unlabeled data
+				classifiedData = new Instances(unlabeledData);
+        		for(int i=0;i<unlabeledData.numInstances();i++){
+                    double classLabel = ttosom.classifyInstance(unlabeledData.instance(i));
+                    classifiedData.instance(i).setClassValue(classLabel);
+        		}
+        }
+		return classifiedData;
+	}
+
+	
+	
+	@SuppressWarnings("unchecked")
+	private JSONObject generateInstancesAsJson(Instances instances){
+		
+		//there's no data to be classified, then return an empty object
+		if(instances == null){
+			return new JSONObject();
+		}
+		
+		JSONObject instancesAsJson = new JSONObject();
+
+		for(int i=0;i<instances.numInstances();i++){
+			JSONArray currentInstanceAsJson = new JSONArray();
+			Instance instance = instances.instance(i);
+			for(int j=0;j<instance.numValues();j++){
+				currentInstanceAsJson.add(j, instance.value(j));
+				instancesAsJson.put(i, currentInstanceAsJson);
+			}
+		}
+		
+		return instancesAsJson;
+		
+	}
+	
+	/*@RequestMapping(value = "/clustering", method=RequestMethod.POST)
+	public ResponseEntity<List<Neuron>> clustering(@RequestBody BasicRequest jsonRequest) throws Exception {
+	}*/
 	
 	//Here we need to see what is the real information that we want to get from cross validation
 	@RequestMapping(value = "/crossValidation", method=RequestMethod.POST)
